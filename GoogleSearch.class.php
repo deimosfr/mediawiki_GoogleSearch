eb5865e9a9f24d1ec5a5f4bfd65522c7a3b7abf3
Deimos <deimos@deimos.fr>
2012-01-05 05:47:56 +0100

    Version 0.3 : Adding a var to avoid editing the code

--- GoogleSearch.class.php
+++ GoogleSearch.class.php
@@ -1,5 +1,4 @@
 <?php
-if (!defined('MEDIAWIKI')) die();
 /**
  * Class file for the GoogleSearch extension
  *
@@ -7,15 +6,19 @@ if (!defined('MEDIAWIKI')) die();
  * @author Pierre Mavro
  * @license GPLv2
  */
+if (!defined('MEDIAWIKI')) die();
+
+# Define default value
+$wgGoogleSearchCSECode ="";
+
 class GoogleSearch {
         static function GoogleSearchInSidebar( $skin, &$bar ) {
-                global $wgGoogleSearchOriginal,$wgGoogleSearchLanguages;
+                global $wgGoogleSearchOriginal,$wgGoogleSearchLanguages,$wgGoogleSearchCSECode;
  
                 wfLoadExtensionMessages( 'Google Search' );
-
                 $bar['googleSearch'] = "<form action=\"http://www.google.fr/cse\" id=\"cse-search-box\">
   <div>
-    <input type=\"hidden\" name=\"cx\" value=\"partner-pub-YOUR_CODE_HERE\" />
+    <input type=\"hidden\" name=\"cx\" value=\"" . $wgGoogleSearchCSECode . "\" />
     <input type=\"hidden\" name=\"ie\" value=\"UTF-8\" />
     <input type=\"text\" name=\"q\" size=\"12\" />
     <input type=\"submit\" name=\"sa\" value=\"" .wfMsg('Search'). "\" />
--- GoogleSearch.php
+++ GoogleSearch.php
@@ -34,7 +34,7 @@ $wgGoogleSearchLanguages  = 'fr,en';        // Languages included in the transla
  
 $wgExtensionCredits['other'][] = array(
         'name'           => 'Google Search',
-        'version'        => '0.2',
+        'version'        => '0.3',
         'author'         => 'Pierre Mavro',
         'description'    => 'Adds Google Search to the sidebar',
         'descriptionmsg' => 'googleSearch-desc',
